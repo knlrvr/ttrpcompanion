8c6b9821c958b21de36e6047cdf6d8a4da8698bc
Kane <kanelariviere@outlook.com>
2023-07-30 23:24:14 -0400

    readd ex router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
-import { exampleRouter } from "y/server/api/routers/example";
-import { createTRPCRouter } from "y/server/api/trpc";
+import { createTRPCRouter } from "../../server/api/trpc";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +6,7 @@ import { createTRPCRouter } from "y/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+
 });
 
 // export type definition of API
