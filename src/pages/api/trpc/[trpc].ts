f3f195de89adf300545af29ae1849792eb00a910
Kane <kanelariviere@outlook.com>
2023-07-30 23:32:23 -0400

    ok i missed a few

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,7 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { env } from "y/env.mjs";
-import { appRouter } from "y/server/api/root";
-import { createTRPCContext } from "y/server/api/trpc";
+import { env } from "@/env.mjs";
+import { appRouter } from "@/server/api/root";
+import { createTRPCContext } from "@/server/api/trpc";
 
 // export API handler
 export default createNextApiHandler({
